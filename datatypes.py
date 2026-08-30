e3301619b4250e2d3dcfe1a0e5e7ab0bda6c39d7
Arpit Chaudhary <arpitchaudhary102006@gmail.com>
2026-08-30 21:49:56 +0530

    Day 2 - functions

--- datatypes.py
+++ datatypes.py
@@ -2,3 +2,21 @@
 print("arpit")
 print(type(-5j))
 print(2e8)
+print(True)
+l1=(23,24,24,25,25)
+print(l1)
+print(type(l1))
+s1={13,13,43,4323,23,14,53}
+print(s1)
+print(type(s1))
+d1={
+    'name':'arpit',
+    'age':20,
+}
+print(d1)
+print(type(d1))
+print('hello world')
+f=238239874
+n=327472307028
+r=f*n
+print(r)
